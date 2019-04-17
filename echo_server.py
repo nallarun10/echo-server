76c0675a31628137602f6aa2a3cc824e26d75ba4
Arun Nalla <nalla.arunkumar@gmail.com>
2019-04-16 23:17:36 -0700

    Class02 assignment

--- echo_server.py
+++ echo_server.py
@@ -4,83 +4,76 @@ import traceback
 
 
 def server(log_buffer=sys.stderr):
-    # set an address for our server
+    # set an address for server
     address = ('127.0.0.1', 10000)
-    # TODO: Replace the following line with your code which will instantiate
-    #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
-    # TODO: You may find that if you repeatedly run the server script it fails,
-    #       claiming that the port is already used.  You can set an option on
-    #       your socket that will fix this problem. We DID NOT talk about this
-    #       in class. Find the correct option by reading the very end of the
-    #       socket library documentation:
-    #       http://docs.python.org/3/library/socket.html#example
 
-    # log that we are building a server
+    """instantiate a TCP socket with IPv4 Addressing, 
+    calling the socket as 'sock'"""
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    """ If repeatedly run the server script it fails,
+    claiming that the port is already used.  Option to fix this problem. 
+    socket library documentation:
+    http://docs.python.org/3/library/socket.html#example"""
+
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  
+     # log that we are building a server
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
 
-    # TODO: bind your new sock 'sock' to the address above and begin to listen
-    #       for incoming connections
-
+    """ bind 'sock' to the address above and begin to listen
+        for incoming connections"""
+    sock.bind(address)
+    sock.listen(1)
     try:
-        # the outer loop controls the creation of new connection sockets. The
-        # server will handle each incoming connection one at a time.
+        """ the outer loop controls the creation of new connection sockets. 
+        The server will handle each incoming connection one at a time."""
         while True:
             print('waiting for a connection', file=log_buffer)
 
-            # TODO: make a new socket when a client connects, call it 'conn',
-            #       at the same time you should be able to get the address of
-            #       the client so we can report it below.  Replace the
-            #       following line with your code. It is only here to prevent
-            #       syntax errors
-            conn, addr = ('foo', ('bar', 'baz'))
+            """ make a new socket when a client connects, call it 'conn',
+            and get get the address of the client""" 
             try:
-                print('connection - {0}:{1}'.format(*addr), file=log_buffer)
+                conn, client_address = sock.accept()
+                print('connection - {0}:{1}'.format(*client_address), file=log_buffer)
 
-                # the inner loop will receive messages sent by the client in
-                # buffers.  When a complete message has been received, the
-                # loop will exit
+                """ Code to receive messages sent by the client in
+                buffers. When a complete message has been received, the
+                loop will exit"""
                 while True:
-                    # TODO: receive 16 bytes of data from the client. Store
-                    #       the data you receive as 'data'.  Replace the
-                    #       following line with your code.  It's only here as
-                    #       a placeholder to prevent an error in string
-                    #       formatting
-                    data = b''
+                    """ Receive 16 bytes of data from the client. Store
+                    the data you receive as 'data'"""
+                    data = conn.recv(16)
                     print('received "{0}"'.format(data.decode('utf8')))
                     
-                    # TODO: Send the data you received back to the client, log
-                    # the fact using the print statement here.  It will help in
-                    # debugging problems.
+                    """ Send the data received back to the client, log
+                    the fact using the print statement here : Help in
+                    debugging problems"""
+                    conn.sendall(data)
                     print('sent "{0}"'.format(data.decode('utf8')))
+
+                    """Check here to see whether you have received the end
+                    of the message. If you have, then break from the `while True`
+                    loop"""
                     
-                    # TODO: Check here to see whether you have received the end
-                    # of the message. If you have, then break from the `while True`
-                    # loop.
-                    # 
-                    # Figuring out whether or not you have received the end of the
-                    # message is a trick we learned in the lesson: if you don't
-                    # remember then ask your classmates or instructor for a clue.
-                    # :)
+                    if len(data) < 16:
+                        print ('All data has been sent')
+                        break
             except Exception as e:
                 traceback.print_exc()
                 sys.exit(1)
             finally:
-                # TODO: When the inner loop exits, this 'finally' clause will
-                #       be hit. Use that opportunity to close the socket you
-                #       created above when a client connected.
-                print(
-                    'echo complete, client connection closed', file=log_buffer
-                )
-
+                """ When the inner loop exits, the finally' clause will
+                close the socket """
+                print('echo complete, client connection closed', file=log_buffer)
+                conn.close()
+                # break
     except KeyboardInterrupt:
-        # TODO: Use the python KeyboardInterrupt exception as a signal to
-        #       close the server socket and exit from the server function.
-        #       Replace the call to `pass` below, which is only there to
-        #       prevent syntax problems
-        pass
+        """ Use the python KeyboardInterrupt exception as a signal to
+        close the server socket and exit from the server function."""
+        # pass
         print('quitting echo server', file=log_buffer)
-
+        sock.close()
 
 if __name__ == '__main__':
     server()
